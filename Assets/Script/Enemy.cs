118c94c0c848dde2a40539387971bf301074b905
JerrysIRL <sergei.maltcev@edu.futuregames.nu>
2023-10-17 20:37:56 +0200

    Optomozed enemyMove function

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -1,33 +1,27 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-   
-    [SerializeField] private int health = 1; 
-    [SerializeField] private float moveSpeed = 2.0f; 
+    [SerializeField] private int health = 1;
+    [SerializeField] private float moveSpeed = 2.0f;
     private Transform _player;
+
     private void Start()
     {
-        // Find the player's transform by tag or other means.
         _player = DataHolder.Instance.GetPlayerTransform();
     }
 
     private void Update()
     {
-        if (_player != null)
-        {
-            Vector3 direction = (_player.position - transform.position).normalized;
-            transform.Translate(direction * (moveSpeed * Time.deltaTime));
-        }
+        transform.position = Vector3.MoveTowards(transform.position, _player.position, moveSpeed * Time.deltaTime);
+         // Vector3 direction = (_player.position - transform.position).normalized;
+         // transform.Translate(direction * (moveSpeed * Time.deltaTime));
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        
-        {
-            Destroy(other.gameObject); // Destroy the projectile.
-            TakeDamage(1); // Inflict damage to the enemy.
-        }
+        TakeDamage(1); // Inflict damage to the enemy.
     }
 
     private void TakeDamage(int damage)
@@ -35,7 +29,8 @@ public class Enemy : MonoBehaviour
         health -= damage;
         if (health <= 0)
         {
-            Destroy(gameObject);
+            EnemySpawner.Instance.ReturnToPool(gameObject);
+            DataHolder.Instance.enemieCounter--;
         }
     }
 }
