5414cc73dc500493382c3516b1b23a34a43833f7
JerrysIRL <sergei.maltcev@edu.futuregames.nu>
2023-10-17 20:37:22 +0200

    Added FPS and Enemie counter UI

--- Assets/Script/DataHolder.cs
+++ Assets/Script/DataHolder.cs
@@ -1,11 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
+
+using TMPro;
 using UnityEngine;
 
+
 public class DataHolder : MonoBehaviour
-{
+{ 
     public static DataHolder Instance { get; private set; }
+    
+    [SerializeField] private TMP_Text enemieAmountText;
+    [SerializeField] private TMP_Text fpsCounterText;
+   
     private Transform _playerTransform;
+    public int enemieCounter;
+    
+    public float updateInterval = 1.0f;
+    private float _timeSinceLastUpdate = 0f;
     private void Awake() 
     { 
         if (Instance != null && Instance != this) 
@@ -20,4 +29,21 @@ public class DataHolder : MonoBehaviour
     }
 
     public Transform GetPlayerTransform() => _playerTransform;
+
+    private void Update()
+    {
+        _timeSinceLastUpdate += Time.deltaTime;
+
+        if (_timeSinceLastUpdate >= updateInterval)
+        {
+            float fps = 1.0f / Time.deltaTime;
+            
+            enemieAmountText.text = $"Enemies Alive: {enemieCounter}";
+            fpsCounterText.text = $"FPS: {Mathf.Round(fps)}";
+            
+            _timeSinceLastUpdate = 0f;
+        }
+
+        
+    }
 }
