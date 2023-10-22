3a66173829a5e1776f92c2eaabc641656f5fba6d
JerrysIRL <sergei.maltcev@edu.futuregames.nu>
2023-10-22 22:36:34 +0200

    Fixed Rotation error in Movement

--- Assets/Script/DOTS/PlayerMoveSystem.cs
+++ Assets/Script/DOTS/PlayerMoveSystem.cs
@@ -32,10 +32,11 @@ namespace Script.DOTS
         private void Execute(ref LocalTransform transform, in PlayerMoveInput moveInput, PlayerMovementParams movementParams)
         {
             var playerMoveInput = moveInput;
-            transform.Position += new float3(0, playerMoveInput.Value.y * (movementParams.MovementSpeed * DeltaTime),0);
+            transform.Position += transform.Up() * playerMoveInput.Value.y * (movementParams.MovementSpeed * DeltaTime);
             playerMoveInput.Value.x = playerMoveInput.Value.y > -0.99f ? -playerMoveInput.Value.x : playerMoveInput.Value.x;
-            
-            //transform.RotateZ(playerMoveInput.Value.x * (movementParams.RotationSpeed * DeltaTime));
+            quaternion tmpRotation = transform.Rotation;
+            tmpRotation = math.mul(tmpRotation, quaternion.RotateZ(playerMoveInput.Value.x * (movementParams.RotationSpeed * DeltaTime)));
+            transform.Rotation = tmpRotation;
             transform.Scale = 1f;
         }
     }
