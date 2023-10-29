6e0a7beeaece5286408174cbd05a0faaacad4034
JerrysIRL <sergei.maltcev@edu.futuregames.nu>
2023-10-29 17:44:28 +0100

    Adjusting font size of UI

--- Assets/Script/WithoutDots/Canon.cs
+++ Assets/Script/WithoutDots/Canon.cs
@@ -6,12 +6,25 @@ public class Canon : MonoBehaviour
 {
     [SerializeField] private GameObject projectilePrefab;
     [SerializeField] private Transform spawnPoint;
+    [SerializeField] private float canonFireRate = 0.3f;
 
-    void FixedUpdate()
+    private float canonTimer;
+
+    private void Start()
+    {
+        canonTimer = canonFireRate;
+    }
+
+    void Update()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            Instantiate(projectilePrefab, spawnPoint.position, spawnPoint.rotation, null);
+            canonTimer -= Time.deltaTime;
+            if (canonTimer <= 0)
+            {
+                Instantiate(projectilePrefab, spawnPoint.position, spawnPoint.rotation, null);
+                canonTimer = canonFireRate;
+            }
         }
     }
 }
